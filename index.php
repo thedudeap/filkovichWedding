f9f4480258a764c7b9bdd945e280540444480255
System Administrator <root@Dans-iMac.local>
2016-03-27 09:46:26 -0400

    First Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Romanovich Wedding 2016</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <style type="text/css">
+
+
+    </style>
+</head>
+<body>
+<h1 style="position:absolute; margin:auto auto;">Romanovich Wedding 2016</h1>
+</body>
+</html>
