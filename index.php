70d8630cd20066abb8cec7aee02a3c68a5824fbc
System Administrator <root@Dans-iMac.local>
2016-03-27 11:18:50 -0400

    test

--- index.php
+++ index.php
@@ -5,11 +5,17 @@
     <title>Romanovich Wedding 2016</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <style type="text/css">
-
+        .backgroundImage{
+            background-image: url('https://s3.amazonaws.com/filkovichwedding/romanovich.jpg');
+            background-position: center;
+            background-size: cover;
+        }
 
     </style>
 </head>
 <body>
+<div class="backgroundImage">
+</div>
 <h1 style="position:absolute; margin:auto auto;">Romanovich Wedding 2016</h1>
 </body>
 </html>
