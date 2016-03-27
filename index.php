fb7f1fd8bc9e4667704b5ad8a11e183181732b9f
System Administrator <root@Dans-iMac.local>
2016-03-27 11:23:17 -0400

    sdf

--- index.php
+++ index.php
@@ -5,10 +5,19 @@
     <title>Romanovich Wedding 2016</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <style type="text/css">
+        html, body{
+            width:100%;
+            height: 100%;
+            margin:0;
+            padding:0;
+        }
         .backgroundImage{
             background-image: url('https://s3.amazonaws.com/filkovichwedding/romanovich.jpg');
             background-position: center;
             background-size: cover;
+            position: absolute;
+            width:100%;
+            height:100%;
         }
 
     </style>
