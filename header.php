b71fc09ca41af6b03d3e355c5dd0d28d0105fe7f
System Administrator <root@Dans-iMac.local>
2016-03-28 09:53:22 -0400

    test

---
+++ header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Romanovich Wedding 2016</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <style type="text/css">
+        html, body{
+            width:100%;
+            height: 100%;
+            margin:0;
+            padding:0;
+        }
+    </style>
+</head>
+<body>
